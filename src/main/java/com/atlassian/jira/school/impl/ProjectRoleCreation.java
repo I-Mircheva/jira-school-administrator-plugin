cc0d9773cff2d493cb24124bc83020c6a53e59a3
DanielRusinov <daniel.rusinov00@gmail.com>
2018-12-20 22:34:48 +0200

    Add call for creating Project Roles

---
+++ src/main/java/com/atlassian/jira/school/impl/ProjectRoleCreation.java
@@ -0,0 +1,29 @@
+package com.atlassian.jira.school.impl;
+
+import com.atlassian.jira.bc.projectroles.ProjectRoleService;
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.security.roles.ProjectRoleImpl;
+import com.atlassian.jira.util.ErrorCollection;
+
+public class ProjectRoleCreation {
+
+	private ProjectRoleService projectRoleService;
+	private ErrorCollection errorCollection;
+	
+	public ProjectRoleCreation() {
+		this.projectRoleService = ComponentAccessor.getComponent(ProjectRoleService.class);
+	}
+	
+	public void FormTeacherProjectRole() {
+    	projectRoleService.createProjectRole(
+    			new ProjectRoleImpl("From-Teacher", "Form-Teacher of a class' devision"), errorCollection);
+	}
+	
+	public void DivisionsProjectRole() {
+    	projectRoleService.createProjectRole(new ProjectRoleImpl("A","Division A"), errorCollection);
+    	projectRoleService.createProjectRole(new ProjectRoleImpl("B","Division B"), errorCollection);
+    	projectRoleService.createProjectRole(new ProjectRoleImpl("V","Division V"), errorCollection);
+    	projectRoleService.createProjectRole(new ProjectRoleImpl("G","Division G"), errorCollection);
+	}
+	
+}
--- src/main/java/com/atlassian/jira/school/rest/ConfigResource.java
+++ src/main/java/com/atlassian/jira/school/rest/ConfigResource.java
@@ -9,38 +9,42 @@ import com.atlassian.crowd.exception.OperationNotPermittedException;
 import com.atlassian.crowd.exception.embedded.InvalidGroupException;
 import com.atlassian.jira.school.impl.GroupCreation;
 import com.atlassian.jira.school.impl.IssueTypeCreation;
-
+import com.atlassian.jira.school.impl.ProjectRoleCreation;
 
 @Path("/setup")
 public class ConfigResource {
 
 	private GroupCreation groupCreation;
 	private IssueTypeCreation issueTypeCreation;
+	private ProjectRoleCreation projectRoleCreation;
 	
 	public ConfigResource() {
 	    this.groupCreation = new GroupCreation();
 	    this.issueTypeCreation = new IssueTypeCreation();
+	    this.projectRoleCreation = new ProjectRoleCreation();
 	}
 	
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response getMessage()
-    {
-       return Response.ok(new ConfigResourceModel("Hello World")).build();
+    public Response getMessage() {
+        return Response.ok(new ConfigResourceModel("Hello World")).build();
     }
     
     @POST
     public Response setupInstance() throws OperationNotPermittedException, InvalidGroupException {
-
-	    issueTypeCreation.subjectIssueType();
-	    issueTypeCreation.lectureIssueType();
-	    issueTypeCreation.homeworkIssueType();
-	    issueTypeCreation.studentRecordIssueType();
-
-        groupCreation.studentsGroup();
+    	
+    	projectRoleCreation.DivisionsProjectRole();
+    	projectRoleCreation.FormTeacherProjectRole();
+        
+    	issueTypeCreation.subjectIssueType();
+    	issueTypeCreation.lectureIssueType();
+    	issueTypeCreation.homeworkIssueType();
+    	issueTypeCreation.studentRecordIssueType();
+        
+    	groupCreation.studentsGroup();
     	groupCreation.teachersGroup();
     	groupCreation.classesGroup();
-
+    	
     	return Response.created(null).build();
     }
 }
