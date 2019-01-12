46969be6bc07c8673e8a6c59214ea0c6969a9f21
DanielRusinov <daniel.rusinov00@gmail.com>
2019-01-12 17:56:24 +0200

    Assign WorkflowScheme to project template

--- src/main/java/com/atlassian/jira/school/impl/AddProjectHookClass.java
+++ src/main/java/com/atlassian/jira/school/impl/AddProjectHookClass.java
@@ -1,10 +1,18 @@
 package com.atlassian.jira.school.impl;
 
+import org.ofbiz.core.entity.GenericEntityException;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.project.Project;
 import com.atlassian.jira.project.template.hook.AddProjectHook;
 import com.atlassian.jira.project.template.hook.ConfigureData;
 import com.atlassian.jira.project.template.hook.ConfigureResponse;
 import com.atlassian.jira.project.template.hook.ValidateData;
 import com.atlassian.jira.project.template.hook.ValidateResponse;
+import com.atlassian.jira.workflow.AssignableWorkflowScheme;
+import com.atlassian.jira.workflow.WorkflowSchemeManager;
+import com.atlassian.jira.workflow.migration.AssignableWorkflowSchemeMigrationHelper;
+import com.atlassian.jira.workflow.migration.MigrationHelperFactory;
 
 public class AddProjectHookClass implements AddProjectHook {
 	@Override
@@ -20,6 +28,22 @@ public class AddProjectHookClass implements AddProjectHook {
 	@Override
 	public ConfigureResponse configure(final ConfigureData configureData) {
 		ConfigureResponse configureResponse = ConfigureResponse.create();
+
+		Project project = configureData.project();
+
+		WorkflowSchemeManager workflowSchemeManager = ComponentAccessor.getWorkflowSchemeManager();
+		AssignableWorkflowScheme workflowScheme = workflowSchemeManager.getWorkflowSchemeObj("Workflow Scheme");
+
+		MigrationHelperFactory migrationHelperFactory = ComponentAccessor
+				.getComponent(MigrationHelperFactory.class);
+
+		try {
+			AssignableWorkflowSchemeMigrationHelper workflowSchemeMigrationHelper = migrationHelperFactory
+					.createMigrationHelper(project, workflowScheme);
+			workflowSchemeMigrationHelper.doQuickMigrate();
+		} catch (GenericEntityException e) {
+			e.printStackTrace();
+		}
 
 		return configureResponse;
 	}
