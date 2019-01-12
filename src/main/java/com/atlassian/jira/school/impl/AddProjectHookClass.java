b9ff7bb3402955959e1753baf3a55eb38d321a94
DanielRusinov <daniel.rusinov00@gmail.com>
2019-01-12 18:05:35 +0200

    Assign IssueTypeScheme to project template

--- src/main/java/com/atlassian/jira/school/impl/AddProjectHookClass.java
+++ src/main/java/com/atlassian/jira/school/impl/AddProjectHookClass.java
@@ -1,9 +1,20 @@
 package com.atlassian.jira.school.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.ofbiz.core.entity.GenericEntityException;
 
 import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueFieldConstants;
+import com.atlassian.jira.issue.context.JiraContextNode;
+import com.atlassian.jira.issue.customfields.CustomFieldUtils;
+import com.atlassian.jira.issue.fields.FieldManager;
+import com.atlassian.jira.issue.fields.config.FieldConfigScheme;
+import com.atlassian.jira.issue.fields.config.manager.FieldConfigSchemeManager;
+import com.atlassian.jira.issue.fields.config.manager.IssueTypeSchemeManager;
 import com.atlassian.jira.project.Project;
+import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.project.template.hook.AddProjectHook;
 import com.atlassian.jira.project.template.hook.ConfigureData;
 import com.atlassian.jira.project.template.hook.ConfigureResponse;
@@ -29,6 +40,8 @@ public class AddProjectHookClass implements AddProjectHook {
 	public ConfigureResponse configure(final ConfigureData configureData) {
 		ConfigureResponse configureResponse = ConfigureResponse.create();
 
+		//assign workflow scheme
+
 		Project project = configureData.project();
 
 		WorkflowSchemeManager workflowSchemeManager = ComponentAccessor.getWorkflowSchemeManager();
@@ -44,6 +57,30 @@ public class AddProjectHookClass implements AddProjectHook {
 		} catch (GenericEntityException e) {
 			e.printStackTrace();
 		}
+
+		//assign issue type scheme
+
+		IssueTypeSchemeManager issueTypeSchemeManager = ComponentAccessor.getIssueTypeSchemeManager();
+		FieldConfigSchemeManager schemeManager = ComponentAccessor.getFieldConfigSchemeManager();
+		ProjectManager projectManager = ComponentAccessor.getProjectManager();
+		FieldManager fieldManager = ComponentAccessor.getFieldManager();
+
+		FieldConfigScheme issueTypeScheme1 = null;
+
+		List<FieldConfigScheme> list = issueTypeSchemeManager.getAllSchemes();
+		for(int i = 0; i < list.size(); i++) {
+			FieldConfigScheme issueTypeScheme = list.get(i);
+			if(issueTypeScheme.getName().equals("School")) issueTypeScheme1 = issueTypeScheme;
+		}
+
+	    List<Long> projectIds = new ArrayList<>(issueTypeScheme1.getAssociatedProjectIds());
+	    projectIds.add(project.getId());
+
+	    List<JiraContextNode> contexts = CustomFieldUtils.buildJiraIssueContexts(false,
+	    		projectIds.toArray(new Long[0]), projectManager);
+
+	    schemeManager.updateFieldConfigScheme(issueTypeScheme1, contexts,
+	    		fieldManager.getConfigurableField(IssueFieldConstants.ISSUE_TYPE));
 
 		return configureResponse;
 	}
