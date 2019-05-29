72a71a1d78f3356f65ce0f9dd0d038143f6566bc
DanielRusinov <daniel.rusinov00@gmail.com>
2019-05-30 00:24:33 +0300

    Fixing workflow creation

--- src/main/java/com/atlassian/jira/school/impl/WorkflowCreation.java
+++ src/main/java/com/atlassian/jira/school/impl/WorkflowCreation.java
@@ -1,16 +1,14 @@
 package com.atlassian.jira.school.impl;
 
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-import com.atlassian.jira.bc.project.ProjectCreationData;
-import com.atlassian.jira.bc.project.ProjectCreationData.Builder;
-import com.atlassian.jira.bc.project.ProjectService;
-import com.atlassian.jira.bc.project.ProjectService.CreateProjectValidationResult;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.config.StatusManager;
+import com.atlassian.jira.issue.status.Status;
 import com.atlassian.jira.issue.status.category.StatusCategory;
-import com.atlassian.jira.project.Project;
-import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.user.ApplicationUser;
 import com.atlassian.jira.workflow.ConfigurableJiraWorkflow;
@@ -19,75 +17,82 @@ import com.atlassian.jira.workflow.WorkflowManager;
 import com.atlassian.jira.workflow.edit.Workflow;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 
-
 @Scanned
 public class WorkflowCreation {
 
-	private ProjectManager projectManager;
-	private ProjectService projectService;
 	private WorkflowManager workflowManager;
 	private StatusManager statusManager;
 	private JiraAuthenticationContext authentication;
 	private ApplicationUser user;
-	
+
 	public WorkflowCreation() {
-		this.projectManager = ComponentAccessor.getProjectManager();
-		this.projectService = ComponentAccessor.getComponent(ProjectService.class);
 		this.workflowManager = ComponentAccessor.getWorkflowManager();
 		this.statusManager = ComponentAccessor.getComponent(StatusManager.class);
 		this.authentication = ComponentAccessor.getJiraAuthenticationContext();
 		this.user = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser();
 	}
-	
+
 	public void createHomeworkWorkflow() {
+		if(workflowManager.getWorkflow("Homework Workflow") == null) {
+			ConfigurableJiraWorkflow confWorkflow = new ConfigurableJiraWorkflow("Homework Workflow", workflowManager);
+			confWorkflow.setDescription("Test workflow!?");
+			
+			workflowManager.createWorkflow(user, confWorkflow);
+			
+			createStatuses();
+		}
+		
+		if(workflowManager.isActive(workflowManager.getWorkflow("Homework Workflow"))) {
+			JiraWorkflow workflow = workflowManager.createDraftWorkflow(user, "Homework Workflow");
+		
+			Workflow newWorkflow = new Workflow(workflow, null, authentication, null, null, null);
+			
+			Collection<Status> statuses = workflow.getLinkedStatusObjects();
+			
+			Map<String, String> map = getStatusesId();
+			
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+				boolean temp = false;
+				for(Iterator<Status> iterator = statuses.iterator(); iterator.hasNext(); ) {
+					Status status = iterator.next();
+					if(status.getName().equals(entry.getKey())) temp = true;
+				}
+				if(!temp) newWorkflow.addStatus(statusManager.getStatus(entry.getValue()));
+			}
+					
 
-		// New project creation - not needed now
-//		ProjectCreationData data = new ProjectCreationData.Builder().withKey("MAX").withName("MAXX2423")
-//				.withLead(user).withDescription("helloworld").withType("business").build();
-//		
-//		CreateProjectValidationResult projectResult = projectService.validateCreateProject(user, data);
-//		projectService.createProject(projectResult);
-		
-		ConfigurableJiraWorkflow confWorkflow = new ConfigurableJiraWorkflow("Homework Workflow", workflowManager);
-		confWorkflow.setDescription("Test workflow!?");
-		
-		workflowManager.createWorkflow(user, confWorkflow);
-		
-//		JiraWorkflow workflow = workflowManager.createDraftWorkflow(user, "Homework Workflow"); //projectManager.getProjectObjects().get(0).getKey() + ": Project Management Workflow");
-//		
-//		workflowManager.updateWorkflowNameAndDescription(user, workflow, "Homework: Workflow", "Trying to create custom workflow");
-//				
-//		Workflow newWorkflow = new Workflow(workflow, null, authentication, null, null, null);
-//		
-//		newWorkflow.addStatus(statusManager.createStatus("Аssigned", "A homework is assigned to a student", StatusCategory.UNDEFINED));
-		
-//		List<Status> list = workflow.getLinkedStatusObjects();
-//		
-//		for(int i = 0; i < list.size(); i++) {
-//			System.out.println(list.get(i).getName() + " " + list.get(i).getSimpleStatus().getId());
-//			StepDescriptor sd = workflow.getLinkedStep(list.get(i));
-//			System.out.println("	step: " + sd.getName() + " " + sd.getId());
-//		}
-
-//		TranslationManager translationManager = ComponentAccessor.getTranslationManager();
 //		StatusCategoryManager statusCategoryManager = ComponentAccessor.getComponent(StatusCategoryManager.class);
 		
 //		newWorkflow.addTransition(AddTransitionParameters.builder().name("Sans").description("Skeleton").sourceStatusId("10000").targetStatusId("10001").build());	
 		
 		
-//		workflowManager.updateWorkflow(user, workflow);
-//		workflowManager.overwriteActiveWorkflow(user, projectManager.getProjectObjects().get(0).getKey() + ": Project Management Workflow");
-//		workflowManager.deleteDraftWorkflow(projectManager.getProjectObjects().get(0).getKey() + ": Project Management Workflow");
-//		
-//		workflow = workflowManager.getWorkflow(projectManager.getProjectObjects().get(0).getKey() + ": Project Management Workflow");
-//		
-//		workflowManager.updateWorkflowNameAndDescription(user, workflow, "Homework: Workflow", "Trying to create custom workflow");
-//		
+			workflowManager.updateWorkflow(user, workflow);
+			workflowManager.overwriteActiveWorkflow(user, "Homework Workflow");
+			workflowManager.deleteDraftWorkflow("Homework Workflow");
+		}
 	}
 	
-	public void createStatus() {
-		
-		
+	public void createStatuses() {
+		statusManager.createStatus("Assigned", "A homework is assigned to a student.",
+				StatusCategory.UNDEFINED);
+		statusManager.createStatus("For revision", "The homework is handed in for revision to a teacher.",
+				StatusCategory.UNDEFINED);
+		statusManager.createStatus("Graded", "The homework is revised and graded by a teacher.",
+				StatusCategory.UNDEFINED);
 	}
-	
+
+	public Map<String, String> getStatusesId() {
+
+		Map<String, String> map = new HashMap<String, String>();
+
+		Collection<Status> statuses = statusManager.getStatuses();
+
+		for (Iterator<Status> iterator = statuses.iterator(); iterator.hasNext();) {
+			Status status = iterator.next();
+			if(status.getName().equals("Closed") || status.getName().equals("Resolved")) continue;
+			map.put(status.getName(), status.getId());
+		}
+
+		return map;
+	}
 }
--- src/main/java/com/atlassian/jira/school/impl/schemes/WorkflowSchemeCreation.java
+++ src/main/java/com/atlassian/jira/school/impl/schemes/WorkflowSchemeCreation.java
@@ -9,24 +9,23 @@ import org.ofbiz.core.entity.GenericValue;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.config.IssueTypeManager;
 import com.atlassian.jira.issue.issuetype.IssueType;
-import com.atlassian.jira.project.Project;
-import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.jira.workflow.WorkflowSchemeManager;
 
 public class WorkflowSchemeCreation {
 
 	private WorkflowSchemeManager workflowSchemeManager;
 	private IssueTypeManager issueTypeManager;
-	private ProjectManager projectManager;
 
 	public WorkflowSchemeCreation() {
 		this.workflowSchemeManager = ComponentAccessor.getWorkflowSchemeManager();
 		this.issueTypeManager = ComponentAccessor.getComponent(IssueTypeManager.class);
-		this.projectManager = ComponentAccessor.getProjectManager();
 	}
 	
+	@SuppressWarnings("deprecation")
 	public void assigneHomeworkIssueTypeToHomeworkWorkflow() throws GenericEntityException {
-		GenericValue scheme = workflowSchemeManager.createScheme("Homework Workflow Scheme", "TEST");
+		GenericValue scheme = workflowSchemeManager.getScheme("Homework Workflow Scheme");
+		
+		if(scheme == null) scheme = workflowSchemeManager.createScheme("Homework Workflow Scheme", "TEST");
 		
 		Collection<IssueType> list = issueTypeManager.getIssueTypes();
 		
